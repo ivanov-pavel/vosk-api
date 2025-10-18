32469cda60fc17829d9201f61077825fdaf99d05
Ivanov Pavel Gennadievich <ivanov.pavel.gennadievich@gmail.com>
2025-10-18 07:33:33 +0300

    Update build-docker-win.sh

--- travis/build-docker-win.sh
+++ travis/build-docker-win.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+set -e
 
-set -e -x 
-docker build --file travis/Dockerfile.win --tag alphacep/kaldi-win:latest .
-docker run --rm -v `realpath ..`:/io alphacep/kaldi-win /io/travis/build-wheels-win.sh
+echo "Building Windows Docker image with cache..."
+
+# Enable BuildKit and caching
+export DOCKER_BUILDKIT=1
+export BUILDKIT_INLINE_CACHE=1
+
+# Build with cache
+docker build \
+    --file travis/Dockerfile.win \
+    --tag alphacep/kaldi-win:latest \
+    --build-arg BUILDKIT_INLINE_CACHE=1 \
+    --cache-from type=gha \
+    --cache-to type=gha,mode=max \
+    .
+
+echo "Docker image built successfully!"
